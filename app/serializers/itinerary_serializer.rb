6252f8660026cee7960833e6d9e704e4eb9e6df6
Bleak Chandler <bleakchandler@gmail.com>
2021-03-30 17:27:20 -0400

    added comments

--- app/serializers/itinerary_serializer.rb
+++ app/serializers/itinerary_serializer.rb
@@ -1,3 +1,5 @@
 class ItinerarySerializer < ActiveModel::Serializer
   attributes :id, :date, :description, :name
+  has_many :itinerary_sites
+  has_many :sites, through: :itinerary_sites
 end
--- app/serializers/site_serializer.rb
+++ app/serializers/site_serializer.rb
@@ -1,3 +1,5 @@
 class SiteSerializer < ActiveModel::Serializer
   attributes :id, :name, :location, :description, :architectural_style, :photo_url
+  has_many :itinerary_sites
+  has_many :itineraries, through: :itinerary_sites
 end
