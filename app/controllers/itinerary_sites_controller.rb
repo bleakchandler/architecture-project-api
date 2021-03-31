764f3ceffe70ff5d27c22ef94a2a8d0fe452f820
Bleak Chandler <bleakchandler@gmail.com>
2021-03-30 20:51:26 -0400

    added some changes

--- app/controllers/itinerary_sites_controller.rb
+++ app/controllers/itinerary_sites_controller.rb
@@ -1,15 +1,17 @@
 class ItinerarySitesController < ApplicationController
 
     def index
-        @itinerary_sites = ItinerarySites.all
+        @itinerary_sites = ItinerarySite.all
         render json: @itinerary_sites
     end
     
     def show 
-        @itinerary_sites = ItinerarySites.find(params[:id])
+        @itinerary_sites = ItinerarySite.find(params[:id])
         render json: @itinerary_sites
     end    
 
-
-   
+    def destroy
+        @itinerary_sites = ItinerarySite.find(params[:id])   
+        @itinerary_sites.destroy 
+    end
 end
